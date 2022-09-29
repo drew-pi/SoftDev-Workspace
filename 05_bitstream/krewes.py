131feba8acaf9da65e67f7be1323cd3ed87b320b
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-09-29 09:13:39 -0400

    finished assn

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,4 +1,25 @@
-import random
+import random as rd
+
+
+'''
+Andrew Piatetsky
+SoftDev
+K05 -- bitstream / Random Picking and creating dictionary with tuples
+2022-09-23
+time spent: 0.5 hours
+
+
+DISCO:
+
+Its very easy to read files in python and then manipulate them.
+string parsing is super nice and fluid too 
+
+
+QCC:
+Can you store objects in files, like can I save my dictionary to an external file or a tuple for example.
+Strings are easy enough to work with but how can we save a list that we were working with 
+
+'''
 
 file_name = "krewes"
 with open(file_name, "r") as f: # f = open...
@@ -7,21 +28,37 @@ with open(file_name, "r") as f: # f = open...
 
 
 def parse(text):
-    devo_text = text.split("@@@")
+    krews_dic = {} # {2:[],7:[],8:[]}
+    devo_text = text.split("@@@") #list of strings
     
-    devo_tuples = []
-    periods = []
+    for strings in devo_text:
+        strings = strings.split('$$$')
+        if int(strings[0]) not in krews_dic:
+            krews_dic[int(strings[0])] = []
+        krews_dic[int(strings[0])].append((strings[1],strings[2]))
     
-    for st in devo_text:
-        devo = st.split('$$$')
-        if devo[0] 
-        devo_tuples.append((devo[0],devo[1],devo[2]))
+    return krews_dic
+
+
+def pick_rand(peeps: dict):
     
+    # get random period
+    rand_period = rd.choice(list(peeps))
+
+    # get random person from the chosen period
+    rand_devo = rd.choice(peeps[rand_period])
+     
+    return f"{rand_period} : {rand_devo[0]} + {rand_devo[1]}"
     
     
 
+  
+krewes = parse(data)
+# print(krewes)
 
-parse(data)
+print(pick_rand(krewes))
+
+
     
 
 
