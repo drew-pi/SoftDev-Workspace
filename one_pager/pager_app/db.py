00d68dd8f6b97d7036900e4b5c6fe7b7847415cb
Andrew Piatetsky <apiatetsky30@stuy.edu>
2023-03-30 14:03:10 -0400

    db code

--- one_pager/pager_app/db.py
+++ one_pager/pager_app/db.py
@@ -1,15 +1,26 @@
 # This file is for reading data from the db/ directory
+import sql
+
 
 # parses text files (only) found in the db directory
 def parse_text(filename):
-
-    file_path = "data/" + filename
-    with open(file_path,"r") as f:
+    FILE_PATH = "data/" + filename
+    with open(FILE_PATH,"r") as f:
         data = f.read()
         f.close()
-
     processed_data = data.split("\n\n\n")
-
     return processed_data
 
-# print(len(parse_text("uses.txt")))
+def connect():
+    DB_FILE="tables.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+    return (c, db)
+
+def disconnect(db):
+    db.commit() #save changes
+    db.close()  #close database
+
+
+
+
