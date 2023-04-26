d2de5bd07b1f5010ad900a89c7edbeedf218a8b1
Andrew Piatetsky <apiatetsky30@stuy.edu>
2023-04-25 21:43:35 -0400

    done

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -17,18 +17,34 @@ var growing = true;
 
 var drawDot = () => {
 
+
     console.log("drawDot invoked")
-    // if (growing == true) {
-    // clear();
+    window.cancelAnimationFrame(requestID);
+
+    clear();
     ctx.beginPath();
     ctx.arc(c.width/2,c.height/2,radius,0,Math.PI*2);
     ctx.fill()
 
     requestID = window.requestAnimationFrame(drawDot);
-    window.cancelAnimationFrame(requestID);
 
-    radius += 1;
-    // };
+    // a bunch of if statements to make sure that it does not grow to crazy
+    if (radius > c.height/2 || radius > c.width/2) {
+        growing = false;
+        console.log("reached border");
+    }
+    if (radius < 1) {
+        growing = true;
+        console.log("reached dot");
+    }
+
+    if (growing == true) {
+        radius += 1;
+    }
+    else {
+        radius -= 1;
+    }
+
 };
 
 
@@ -36,9 +52,11 @@ var stopIt = () => {
     console.log("stopIT invoked...");
     console.log( requestID );
 
-    growing = false;
-    window.cancelAnimationFrame();
+    // growing = false;
+    window.cancelAnimationFrame(requestID);
 };
 
 dotButton.addEventListener("click", drawDot);
 stopButton.addEventListener("click", stopIt );
+
+
